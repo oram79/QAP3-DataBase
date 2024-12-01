5fa3f978f6fc7113c9a930fd7ea47edf95dd86b1
Logan Oram <logan.oram@keyin.com>
2024-11-30 22:28:01 -0330

    database connected
    using .env

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const { Pool } = require('pg');
+require('dotenv').config();
+
+// PostgreSQL connection
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+  module.exports = pool;
 
 app.use(express.json());
 
